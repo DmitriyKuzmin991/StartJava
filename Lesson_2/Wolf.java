e9c748a058c59ca2805172b1ce72b5227ac67440
DmitriyK <Dmitriykuzminn@gmail.com>
2024-03-10 20:39:24 +0300

    I'm traning use encapsulation in class Wolf and test it in class wolfTest

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -2,12 +2,46 @@ package Lesson_2;
 
 public class Wolf {
     
-    String sex;
-    String nickName;
-    int weight;
-    int age;
-    String colorSkin;
+    private String sex;
+    private String nickName;
+    private int weight;
+    private int age;
+    private String colorSkin;
 
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getSex() {
+        return sex;
+    }
+    public void setColorSkin(String color) {
+        this.colorSkin = color;
+    }
+    public String getColorSkin() {
+        return colorSkin;
+    }
+    public void setNickname(String nickName) {
+        this.nickName = nickName;
+    }
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Ошибка, слишком большой возраст.");
+        } else {
+            this.age = age;
+        }
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getWeight() {
+        return weight;
+    }
     public void walk() {System.out.println("Волк пошёл");}
     public void sitdown() {System.out.println("Волк сел");}
     public void run() {System.out.println("Волк побежал");}
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -3,16 +3,14 @@ package Lesson_2;
 public class WolfTest {
     public static void main(String[] args) {
         Wolf w = new Wolf();
-        w.nickName = "Seeker";
-        w.age = 5;
-        w.colorSkin = "black";
-        w.sex = "male";
-        w.weight = 35;
-        System.out.printf("The %s wolf is %d years old and has %s skin%n", w.nickName, w.age, w.colorSkin);
+        w.setAge(5);
+        w.setAge(9);
+        w.setNickname("Bobick");
         w.walk();
         w.run();
         w.hunt();
         w.sitdown();
+        System.out.println(w.getNickName());
 
     }
 }
